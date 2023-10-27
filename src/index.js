e966f496a56908da4fcb5469e6e4186f15bce900
HeD96 <hedplanet96@gmail.com>
2023-10-27 22:22:08 +0300

    reverse, do again

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import "./style.css";
-import foodImage from "./food.jpg";
-
-const mainDiv = document.querySelector("#content");
-mainDiv.classList.add("text-color");
-
-const bkgImage = new Image();
-bkgImage.src = foodImage;
