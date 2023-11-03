941da1159642bedb5d57468e3e02ba0b6cd586b5
HeD96 <hedplanet96@gmail.com>
2023-11-03 21:13:43 +0300

    finished but code is in need of review, might do it later

--- src/menuTab.js
+++ src/menuTab.js
@@ -17,38 +17,91 @@ export function MenuTab() {
         const categoryName = document.createElement("h1");
         const categoryContent = document.createElement("ul");
 
-        for (let i = 0; i < 3; i++) {
-            const dish = document.createElement("li");
-            dish.textContent = "Keke";
-            categoryContent.appendChild(dish);
-        }
-
-        MenuCreation(categoryName, i);
+        Menu.menuCreation(categoryName, i, categoryContent);
 
         menuPage.appendChild(category);
         category.append(categoryName, categoryContent);
     }
 }
 
-function MenuCreation(category, categoryNum) {
-    switch (categoryNum) {
-        case 1:
-            category.innerText = "Firsts";
-            break;
-        case 2:
-            category.innerText = "Seconds";
-            break;
-        case 3:
-            category.innerText = "Salads";
-            break;
-        case 4:
-            category.innerText = "Desserts";
-            break;
-        case 5:
-            category.innerText = "Drinks";
-            break;
-        case 6:
-            category.innerText = "Specials";
-            break;
-    }
-}
+const Menu = (function () {
+    let firsts = ["Borscht", "Chicken Soup", "Pickle Soup"];
+    let seconds = [
+        "Cutlets with Mashed Potato",
+        "Chicken with Rice",
+        "Beef with Vegetables",
+    ];
+    let salads = ["Olivier Salad", "Greek Salad", "Caesar Salad"];
+    let desserts = ["Doughnuts", "Cheesecake", "Medovik"];
+    let drinks = ["Juice (in assortment)", "Tea", "Coffee", "Wine"];
+    let specials = ["Chef's kiss", "Awesomesauce", "Pure Gold"];
+
+    const menuCreation = function (title, categoryIndex, menu) {
+        switch (categoryIndex) {
+            case 1:
+                title.innerText = "Firsts";
+
+                for (let i = 0; i < firsts.length; i++) {
+                    const dish = document.createElement("li");
+                    dish.textContent = firsts[i];
+                    menu.appendChild(dish);
+                }
+
+                break;
+            case 2:
+                title.innerText = "Seconds";
+
+                for (let i = 0; i < seconds.length; i++) {
+                    const dish = document.createElement("li");
+                    dish.textContent = seconds[i];
+                    menu.appendChild(dish);
+                }
+
+                break;
+            case 3:
+                title.innerText = "Salads";
+
+                for (let i = 0; i < salads.length; i++) {
+                    const dish = document.createElement("li");
+                    dish.textContent = salads[i];
+                    menu.appendChild(dish);
+                }
+
+                break;
+            case 4:
+                title.innerText = "Desserts";
+
+                for (let i = 0; i < desserts.length; i++) {
+                    const dish = document.createElement("li");
+                    dish.textContent = desserts[i];
+                    menu.appendChild(dish);
+                }
+
+                break;
+            case 5:
+                title.innerText = "Drinks";
+
+                for (let i = 0; i < drinks.length; i++) {
+                    const dish = document.createElement("li");
+                    dish.textContent = drinks[i];
+                    menu.appendChild(dish);
+                }
+
+                break;
+            case 6:
+                title.innerText = "Specials";
+
+                for (let i = 0; i < specials.length; i++) {
+                    const dish = document.createElement("li");
+                    dish.textContent = specials[i];
+                    menu.appendChild(dish);
+                }
+
+                break;
+        }
+    };
+
+    return {
+        menuCreation,
+    };
+})();
